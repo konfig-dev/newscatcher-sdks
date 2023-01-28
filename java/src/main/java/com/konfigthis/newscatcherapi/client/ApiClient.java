04f06179068b8dbf722e3e39b45e355041ba4a0c
dphuang2 <dylan.p.huang@gmail.com>
2023-01-28 08:48:44 -0800

    bump/regenerate Java SDK

--- java/src/main/java/com/konfigthis/newscatcherapi/client/ApiClient.java
+++ java/src/main/java/com/konfigthis/newscatcherapi/client/ApiClient.java
@@ -130,7 +130,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/1.0.0/java");
+        setUserAgent("Konfig/1.1.0/java");
 
         authentications = new HashMap<String, Authentication>();
     }
