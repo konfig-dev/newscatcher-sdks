88393fec878bea5220eb8a4031ff9095b6b98a7b
dphuang2 <dylan.p.huang@gmail.com>
2023-01-31 06:53:51 -0800

    regenerate go SDK

--- go/configuration.go
+++ go/configuration.go
@@ -104,7 +104,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "Konfig/2.0.0/go",
+		UserAgent:        "Konfig/2.1.0/go",
 		Debug:            false,
         Context: context.Background(),
 		Servers:          ServerConfigurations{
