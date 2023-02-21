92fb0a676e85e55f5dc32d61016fb2aedb14c264
dphuang2 <dylan.p.huang@gmail.com>
2023-02-20 16:30:15 -0800

    regenerate csharp SDK

--- csharp/src/Newscatcherapi.Net/Client/Configuration.cs
+++ csharp/src/Newscatcherapi.Net/Client/Configuration.cs
@@ -33,7 +33,7 @@ namespace Newscatcherapi.Net.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "2.0.0";
+        public const string Version = "2.1.0";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -110,7 +110,7 @@ namespace Newscatcherapi.Net.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("Konfig/2.0.0/csharp");
+            UserAgent = WebUtility.UrlEncode("Konfig/2.1.0/csharp");
             BasePath = "https://api.newscatcherapi.com/v2";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -518,7 +518,7 @@ namespace Newscatcherapi.Net.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0.1\n";
-            report += "    SDK Package Version: 2.0.0\n";
+            report += "    SDK Package Version: 2.1.0\n";
 
             return report;
         }
